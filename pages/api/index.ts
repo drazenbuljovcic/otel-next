c741f7e0c4e88a228f0dd0f927964b0a74a29309
Drazen Buljovcic <drazen.buljovcic@gmail.com>
2022-11-04 11:11:03 +0100

    fix api route

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,10 +2,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import withOpenTelemetry from "../../src/server/withOpenTelemetry";
 import client from "../../src/server/api/client";
+import { AxiosError } from "axios";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await client.get("http://localhost:3001/api");
-  return res.status(200).json(response.data);
+  try {
+    const response = await client.get("http://localhost:3001/api");
+    return res.status(200).json(response.data);
+  } catch (thrown) {
+    const error = thrown as AxiosError;
+    return res
+      .status(error.status || 500)
+      .json({ message: error.response?.statusText });
+  }
 };
 
 export default withOpenTelemetry(handler);
